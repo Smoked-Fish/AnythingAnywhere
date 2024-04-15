4cfdf6f26b17f9fc12fad1226d841b0813cde6f7
Smoked Fish <espy.jacob@gmail.com>
2024-04-15 05:16:12 -0700

    BuildAnywhereMenu refactor

--- Framework/UI/BuildAnywhereMenu.cs
+++ Framework/UI/BuildAnywhereMenu.cs
@@ -1,9 +1,8 @@
 ﻿using StardewValley;
 using StardewModdingAPI;
-using StardewValley.Buildings;
 using StardewValley.GameData.Buildings;
 using StardewValley.Menus;
-using Microsoft.Xna.Framework;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 
 namespace AnythingAnywhere.Framework.UI
@@ -13,15 +12,12 @@ namespace AnythingAnywhere.Framework.UI
         public BuildAnywhereMenu(string builder, ModConfig config, IMonitor monitor) : base(builder, Game1.currentLocation)
         {
             TargetLocation = Game1.currentLocation;
-            Game1.player.forceCanMove();
-            resetBounds();
             int index = 0;
             Blueprints.Clear();
             foreach (KeyValuePair<string, BuildingData> data in Game1.buildingData)
             {
 
-                if ((data.Value.Builder != builder || !GameStateQuery.CheckConditions(data.Value.BuildCondition, TargetLocation) || data.Value.BuildingToUpgrade != null && TargetLocation.getNumberBuildingsConstructed(data.Value.BuildingToUpgrade) == 0 || !IsValidBuildingForLocation(data.Key, data.Value, TargetLocation)) &&
-                    !config.EnableInstantBuild)
+                if ((data.Value.Builder != builder || !GameStateQuery.CheckConditions(data.Value.BuildCondition, TargetLocation) || data.Value.BuildingToUpgrade != null && TargetLocation.getNumberBuildingsConstructed(data.Value.BuildingToUpgrade) == 0 || !IsValidBuildingForLocation(data.Key, data.Value, TargetLocation)) && !config.EnableInstantBuild)
                 {
                     continue;
                 }
@@ -29,12 +25,19 @@ namespace AnythingAnywhere.Framework.UI
                 {
                     if (data.Value.Builder != builder)
                         continue;
-                    data.Value.MagicalConstruction = true;
-                    data.Value.BuildCost = 0;
-                    data.Value.BuildDays = 0;
-                    data.Value.BuildMaterials = new List<BuildingMaterial>();
+                    BuildingData copyData = DeepCopy(data.Value);
+
+                    copyData.MagicalConstruction = true;
+                    copyData.BuildCost = 0;
+                    copyData.BuildDays = 0;
+                    copyData.BuildMaterials = [];
+
+                    Blueprints.Add(new BlueprintEntry(index++, data.Key, copyData, null));
                 }
-                Blueprints.Add(new BlueprintEntry(index++, data.Key, data.Value, null));
+                else
+                {
+                    Blueprints.Add(new BlueprintEntry(index++, data.Key, data.Value, null));
+                }
                 /*monitor.LogOnce($"Blueprint Added. Index: {index}\nData: {data.Key}\nData: {data.Value}", LogLevel.Info);*/
                 if (data.Value.Skins == null)
                 {
@@ -48,110 +51,16 @@ namespace AnythingAnywhere.Framework.UI
                     }
                 }
             }
-
             SetNewActiveBlueprint(0);
-            if (Game1.options.SnappyMenus)
-            {
-                populateClickableComponentList();
-                snapToDefaultClickableComponent();
-            }
         }
 
-        private void resetBounds()
+        private BuildingData DeepCopy(BuildingData source)
         {
-            xPositionOnScreen = Game1.uiViewport.Width / 2 - maxWidthOfBuildingViewer - spaceToClearSideBorder;
-            yPositionOnScreen = Game1.uiViewport.Height / 2 - maxHeightOfBuildingViewer / 2 - spaceToClearTopBorder + 32;
-            width = maxWidthOfBuildingViewer + maxWidthOfDescription + spaceToClearSideBorder * 2 + 64;
-            height = maxHeightOfBuildingViewer + spaceToClearTopBorder;
-            initialize(xPositionOnScreen, yPositionOnScreen, width, height, showUpperRightCloseButton: true);
-            okButton = new ClickableTextureComponent("OK", new Rectangle(xPositionOnScreen + width - borderWidth - spaceToClearSideBorder - 192 - 12, yPositionOnScreen + maxHeightOfBuildingViewer + 64, 64, 64), null, null, Game1.mouseCursors, new Rectangle(366, 373, 16, 16), 4f)
-            {
-                myID = 106,
-                rightNeighborID = 104,
-                leftNeighborID = 105,
-                upNeighborID = 109
-            };
-            cancelButton = new ClickableTextureComponent("OK", new Rectangle(xPositionOnScreen + width - borderWidth - spaceToClearSideBorder - 64, yPositionOnScreen + maxHeightOfBuildingViewer + 64, 64, 64), null, null, Game1.mouseCursors, Game1.getSourceRectForStandardTileSheet(Game1.mouseCursors, 47), 1f)
-            {
-                myID = 107,
-                leftNeighborID = 104,
-                upNeighborID = 109
-            };
-            backButton = new ClickableTextureComponent(new Rectangle(xPositionOnScreen + 64, yPositionOnScreen + maxHeightOfBuildingViewer + 64, 48, 44), Game1.mouseCursors, new Rectangle(352, 495, 12, 11), 4f)
-            {
-                myID = 101,
-                rightNeighborID = 102,
-                upNeighborID = 109
-            };
-            forwardButton = new ClickableTextureComponent(new Rectangle(xPositionOnScreen + maxWidthOfBuildingViewer - 256 + 16, yPositionOnScreen + maxHeightOfBuildingViewer + 64, 48, 44), Game1.mouseCursors, new Rectangle(365, 495, 12, 11), 4f)
-            {
-                myID = 102,
-                leftNeighborID = 101,
-                rightNeighborID = -99998,
-                upNeighborID = 109
-            };
-            demolishButton = new ClickableTextureComponent(Game1.content.LoadString("Strings\\UI:Carpenter_Demolish"), new Rectangle(xPositionOnScreen + width - borderWidth - spaceToClearSideBorder - 128 - 8, yPositionOnScreen + maxHeightOfBuildingViewer + 64 - 4, 64, 64), null, null, Game1.mouseCursors, new Rectangle(348, 372, 17, 17), 4f)
-            {
-                myID = 104,
-                rightNeighborID = 107,
-                leftNeighborID = 106,
-                upNeighborID = 109
-            };
-            upgradeIcon = new ClickableTextureComponent(new Rectangle(xPositionOnScreen + maxWidthOfBuildingViewer - 128 + 32, yPositionOnScreen + 8, 36, 52), Game1.mouseCursors, new Rectangle(402, 328, 9, 13), 4f)
-            {
-                myID = 103,
-                rightNeighborID = 104,
-                leftNeighborID = 105,
-                upNeighborID = 109
-            };
-            moveButton = new ClickableTextureComponent(Game1.content.LoadString("Strings\\UI:Carpenter_MoveBuildings"), new Rectangle(xPositionOnScreen + width - borderWidth - spaceToClearSideBorder - 256 - 20, yPositionOnScreen + maxHeightOfBuildingViewer + 64, 64, 64), null, null, Game1.mouseCursors, new Rectangle(257, 284, 16, 16), 4f)
-            {
-                myID = 105,
-                rightNeighborID = 106,
-                leftNeighborID = -99998,
-                upNeighborID = 109
-            };
-            paintButton = new ClickableTextureComponent(Game1.content.LoadString("Strings\\UI:Carpenter_PaintBuildings"), new Rectangle(xPositionOnScreen + width - borderWidth - spaceToClearSideBorder - 320 - 20, yPositionOnScreen + maxHeightOfBuildingViewer + 64, 64, 64), null, null, Game1.mouseCursors2, new Rectangle(80, 208, 16, 16), 4f)
-            {
-                myID = 105,
-                rightNeighborID = -99998,
-                leftNeighborID = -99998,
-                upNeighborID = 109
-            };
-            appearanceButton = new ClickableTextureComponent(Game1.content.LoadString("Strings\\UI:Carpenter_ChangeAppearance"), new Rectangle(xPositionOnScreen + maxWidthOfBuildingViewer - 128 + 16, yPositionOnScreen + maxHeightOfBuildingViewer - 64 + 32, 64, 64), null, null, Game1.mouseCursors2, new Rectangle(96, 208, 16, 16), 4f)
-            {
-                myID = 109,
-                downNeighborID = -99998
-            };
-            bool has_owned_buildings = false;
-            bool has_paintable_buildings = false;
-            foreach (Building building in TargetLocation.buildings)
-            {
-                if (building.hasCarpenterPermissions())
-                {
-                    has_owned_buildings = true;
-                }
-                if ((building.CanBePainted() || building.CanBeReskinned(ignoreSeparateConstructionEntries: true)) && HasPermissionsToPaint(building))
-                {
-                    has_paintable_buildings = true;
-                }
-            }
-            demolishButton.visible = Game1.IsMasterGame;
-            moveButton.visible = Game1.IsMasterGame || Game1.player.team.farmhandsCanMoveBuildings.Value == FarmerTeam.RemoteBuildingPermissions.On || Game1.player.team.farmhandsCanMoveBuildings.Value == FarmerTeam.RemoteBuildingPermissions.OwnedBuildings && has_owned_buildings;
-            paintButton.visible = has_paintable_buildings;
-            if (!demolishButton.visible)
-            {
-                upgradeIcon.rightNeighborID = demolishButton.rightNeighborID;
-                okButton.rightNeighborID = demolishButton.rightNeighborID;
-                cancelButton.leftNeighborID = demolishButton.leftNeighborID;
-            }
-            if (!moveButton.visible)
-            {
-                upgradeIcon.leftNeighborID = moveButton.leftNeighborID;
-                forwardButton.rightNeighborID = -99998;
-                okButton.leftNeighborID = moveButton.leftNeighborID;
-            }
-            UpdateAppearanceButtonVisibility();
+            if (source == null)
+                return null;
+
+            string serializedObject = JsonConvert.SerializeObject(source);
+            return JsonConvert.DeserializeObject<BuildingData>(serializedObject);
         }
     }
 }
