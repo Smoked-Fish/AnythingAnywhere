b92ec5e8426263ee6278882f6025a3435857ccb1
Smoked Fish <espy.jacob@gmail.com>
2024-04-26 18:59:32 -0700

    disable CS0649 warning

--- Framework/UI/BuildAnywhereMenu.cs
+++ Framework/UI/BuildAnywhereMenu.cs
@@ -8,7 +8,9 @@ namespace AnythingAnywhere.Framework.UI
     internal class BuildAnywhereMenu : CarpenterMenu
     {
         // Prevents Better Juminos from spamming errors
+        #pragma warning disable CS0649
         public bool magicalConstruction;
+        #pragma warning restore CS0649
 
         public BuildAnywhereMenu(string builder, GameLocation targetLocation = null) : base(builder, targetLocation)
         {
