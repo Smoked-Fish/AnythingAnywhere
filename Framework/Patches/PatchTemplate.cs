e6168dbe83f689e1860b44988fad22b537810f5e
Smoked Fish <espy.jacob@gmail.com>
2024-05-02 08:43:25 -0700

    bool to enum and error handling

--- Framework/Patches/PatchTemplate.cs
+++ Framework/Patches/PatchTemplate.cs
@@ -17,19 +17,46 @@ namespace AnythingAnywhere.Framework.Patches
             _object = objectType;
         }
 
+
         /// <summary>
         /// Applies method patches using Harmony for a specified target method.
         /// </summary>
-        /// <param name="isPostfix">0 for postfix, 1 for prefix.</param>
+        /// <param name="patchType">The type of patch to apply: Prefix, Postfix, or Transpiler.</param>
         /// <param name="originalMethod">The name of the original method to patch.</param>
         /// <param name="newMethod">The name of the method to be applied as a patch.</param>
         /// <param name="parameters">Optional parameters for the method.</param>
-        public void Patch(bool isPostfix, string originalMethod, string newMethod, Type[] parameters = null)
+        public void Patch(PatchType patchType, string originalMethod, string newMethod, Type[] parameters = null)
         {
-            if (isPostfix == true)
-                _harmony.Patch(AccessTools.Method(_object, originalMethod, parameters), postfix: new HarmonyMethod(GetType(), newMethod));
-            else if (isPostfix == false)
-                _harmony.Patch(AccessTools.Method(_object, originalMethod, parameters), prefix: new HarmonyMethod(GetType(), newMethod));
+            try
+            {
+                switch (patchType)
+                {
+                    case PatchType.Prefix:
+                        _harmony.Patch(AccessTools.Method(_object, originalMethod, parameters), prefix: new HarmonyMethod(GetType(), newMethod));
+                        break;
+                    case PatchType.Postfix:
+                        _harmony.Patch(AccessTools.Method(_object, originalMethod, parameters), postfix: new HarmonyMethod(GetType(), newMethod));
+                        break;
+                    case PatchType.Transpiler:
+                        _harmony.Patch(AccessTools.Method(_object, originalMethod, parameters), transpiler: new HarmonyMethod(GetType(), newMethod));
+                        break;
+                    default:
+                        ModEntry.monitor.Log($"Unknown patch type: {patchType}", LogLevel.Error);
+                        return;
+                }
+            }
+            catch (Exception e)
+            {
+                string errorMessage = $"Issue with Harmony patching for method {originalMethod} with {newMethod}: {e}";
+                ModEntry.monitor.Log(errorMessage, LogLevel.Error);
+            }
+        }
+
+        public enum PatchType
+        {
+            Prefix,
+            Postfix,
+            Transpiler
         }
     }
 }
