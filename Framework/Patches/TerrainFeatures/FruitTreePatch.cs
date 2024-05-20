73605a37b659d524556ee09770d264f42c0ef933
Smoked Fish <espy.jacob@gmail.com>
2024-05-20 00:32:17 -0700

    IsTooCloseToAnotherTreePostfix

--- Framework/Patches/TerrainFeatures/FruitTreePatch.cs
+++ Framework/Patches/TerrainFeatures/FruitTreePatch.cs
@@ -11,9 +11,17 @@ namespace AnythingAnywhere.Framework.Patches.TerrainFeatures
         internal void Apply()
         {
             Patch(PatchType.Postfix, nameof(FruitTree.IsGrowthBlocked), nameof(IsGrowthBlockedPostfix), [typeof(Vector2), typeof(GameLocation)]);
+            Patch(PatchType.Postfix, nameof(FruitTree.IsTooCloseToAnotherTree), nameof(IsTooCloseToAnotherTreePostfix), [typeof(Vector2), typeof(GameLocation), typeof(bool)]);
+
         }
 
         public static void IsGrowthBlockedPostfix(FruitTree __instance, Vector2 tileLocation, GameLocation environment, ref bool __result)
+        {
+            if (ModEntry.Config.EnableFruitTreeTweaks)
+                __result = false;
+        }
+
+        private static void IsTooCloseToAnotherTreePostfix(FruitTree __instance, Vector2 tileLocation, GameLocation environment, ref bool __result, bool fruitTreesOnly = false)
         {
             if (ModEntry.Config.EnableFruitTreeTweaks)
                 __result = false;
