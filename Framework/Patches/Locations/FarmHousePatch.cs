22bd0943b4d8fae348947f04df26d4e021ac0e75
Smoked Fish <espy.jacob@gmail.com>
2024-05-20 00:27:15 -0700

    get player pos before warp

--- Framework/Patches/Locations/FarmHousePatch.cs
+++ Framework/Patches/Locations/FarmHousePatch.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using Microsoft.Xna.Framework;
 using StardewValley;
 using StardewValley.Locations;
 
@@ -6,26 +7,16 @@ namespace AnythingAnywhere.Framework.Patches.Locations
 {
     internal class FarmHousePatch : PatchTemplate
     {
+        public static Vector2 FarmHouseRealPos {  get; set; }
         internal FarmHousePatch(Harmony harmony) : base(harmony, typeof(FarmHouse)) { }
         internal void Apply()
         {
             Patch(PatchType.Prefix, "resetLocalState", nameof(ResetLocalStatePrefix));
         }
 
-        private static bool ResetLocalStatePrefix(FarmHouse __instance)
+        private static void ResetLocalStatePrefix(FarmHouse __instance)
         {
-            if (Game1.player.currentLocation.Name.StartsWith("ScienceHouse") || Game1.player.currentLocation.Name.EndsWith("ScienceHouse"))
-                return true;
-
-            if (!Game1.player.currentLocation.IsOutdoors && !(Game1.player.currentLocation is FarmHouse || Game1.player.currentLocation is Cabin))
-            {
-                //Game1.player.Position = Utility.PointToVector2(getEntryLocation()) * 64f;
-                Game1.xLocationAfterWarp = Game1.player.TilePoint.X;
-                Game1.yLocationAfterWarp = Game1.player.TilePoint.Y;
-                Game1.player.currentLocation = __instance;
-                return false;
-            }
-            return true;
+            FarmHouseRealPos = Game1.player.Tile;
         }
     }
 }
