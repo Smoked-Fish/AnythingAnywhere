bff642dcc5cf9f8724da7ec7d9526ab19eea8008
Smoked Fish <espy.jacob@gmail.com>
2024-05-28 05:27:12 -0700

    use primary constructor

--- Framework/UI/BuildAnywhereMenu.cs
+++ Framework/UI/BuildAnywhereMenu.cs
@@ -1,28 +1,20 @@
-﻿using Common.Managers;
+﻿#nullable disable
 using StardewValley;
-using StardewValley.GameData.Buildings;
 using StardewValley.Menus;
+using StardewValley.GameData.Buildings;
 
 namespace AnythingAnywhere.Framework.UI
 {
-    internal class BuildAnywhereMenu : CarpenterMenu
+    internal sealed class BuildAnywhereMenu(string builder, GameLocation targetLocation = null) : CarpenterMenu(builder, targetLocation)
     {
         // Prevents Better Juminos from spamming errors
         #pragma warning disable CS0649
         public bool magicalConstruction;
-        #pragma warning restore CS0649
-
-        public BuildAnywhereMenu(string builder, GameLocation targetLocation = null) : base(builder, targetLocation) { }
 
         // Check if a building is valid for a location
         public override bool IsValidBuildingForLocation(string typeId, BuildingData data, GameLocation targetLocation)
         {
-            if ((typeId == "Cabin" && TargetLocation.Name != "Farm") && Game1.IsMultiplayer)
-            {
-                return false;
-            }
-
-            return true;
+            return typeId != "Cabin" || TargetLocation.Name == "Farm" || !Game1.IsMultiplayer;
         }
     }
 }
