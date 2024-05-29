5e036baf2e145c749441c8d7130e33770864c38e
Smoked Fish <espy.jacob@gmail.com>
2024-05-29 00:05:43 -0700

    TODO's and less nested statements

--- Framework/Patches/Menus/AnimalQueryMenuPatch.cs
+++ Framework/Patches/Menus/AnimalQueryMenuPatch.cs
@@ -1,24 +1,23 @@
 ﻿#nullable disable
-using HarmonyLib;
-using StardewValley;
-using StardewModdingAPI;
-using StardewValley.Menus;
-using StardewValley.Buildings;
+using Common.Helpers;
+using Common.Utilities;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
-using xTile.Dimensions;
-using Common.Managers;
-using Common.Helpers;
-using System.Reflection;
+using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Buildings;
+using StardewValley.Menus;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using xTile.Dimensions;
 
 namespace AnythingAnywhere.Framework.Patches.Menus
 {
     internal sealed class AnimalQueryMenuPatch : PatchHelper
     {
         private static GameLocation TargetLocation;
-        internal AnimalQueryMenuPatch(Harmony harmony) : base(harmony, typeof(AnimalQueryMenu)) { }
+        internal AnimalQueryMenuPatch() : base(typeof(AnimalQueryMenu)) { }
         internal void Apply()
         {
             Patch(PatchType.Prefix, nameof(AnimalQueryMenu.receiveLeftClick), nameof(ReceiveLeftClickPrefix), [typeof(int), typeof(int), typeof(bool)]);
@@ -35,8 +34,10 @@ namespace AnythingAnywhere.Framework.Patches.Menus
             if (Game1.globalFade)
                 return true;
 
-            bool movingAnimal = (bool)typeof(AnimalQueryMenu).GetField("movingAnimal", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance);
-            FarmAnimal animal = (FarmAnimal)typeof(AnimalQueryMenu).GetField("animal", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance);
+            // TODO: FIX THIS AND ADD REFLECTION HELPER TO COMMON
+            bool movingAnimal = (bool)typeof(AnimalQueryMenu).GetField("movingAnimal", BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(__instance)!;
+            FarmAnimal animal = (FarmAnimal)typeof(AnimalQueryMenu).GetField("animal", BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(__instance);
+            if (animal is null) return false;
 
             if (movingAnimal)
             {
@@ -45,7 +46,7 @@ namespace AnythingAnywhere.Framework.Patches.Menus
                     Game1.globalFadeToBlack(__instance.prepareForReturnFromPlacement);
                     Game1.playSound("smallSelect");
                 }
-                Vector2 clickTile = new((Game1.viewport.X + Game1.getOldMouseX(ui_scale: false)) / 64, (Game1.viewport.Y + Game1.getOldMouseY(ui_scale: false)) / 64);
+                Vector2 clickTile = new((float)(Game1.viewport.X + Game1.getOldMouseX(ui_scale: false)) / 64, (float)(Game1.viewport.Y + Game1.getOldMouseY(ui_scale: false)) / 64);
                 Building selection = TargetLocation.getBuildingAt(clickTile);
                 if (selection == null)
                 {
@@ -80,60 +81,56 @@ namespace AnythingAnywhere.Framework.Patches.Menus
                 return false;
             }
 
-            bool confirmingSell = (bool)typeof(AnimalQueryMenu).GetField("confirmingSell", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance);
+            bool confirmingSell = (bool)typeof(AnimalQueryMenu).GetField("confirmingSell", BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(__instance)!;
 
             if (confirmingSell)
                 return true;
 
-            if (__instance.moveHomeButton.containsPoint(x, y))
+            if (!__instance.moveHomeButton.containsPoint(x, y)) return true;
+            List<KeyValuePair<string, string>> validLocations = [];
+            Game1.playSound("smallSelect");
+
+            // ReSharper disable once LoopCanBeConvertedToQuery -- so I can actually read the damn thing
+            foreach (GameLocation location in Game1.locations)
             {
-                List<KeyValuePair<string, string>> validLocations = [];
-
-                Game1.playSound("smallSelect");
-                foreach (GameLocation location in Game1.locations)
+                if (location.buildings.Any(p => p.GetIndoors() is AnimalHouse) && (!Game1.IsClient || location.CanBeRemotedlyViewed()))
                 {
-                    if (location.buildings.Any((Building p) => p.GetIndoors() is AnimalHouse) && (!Game1.IsClient || location.CanBeRemotedlyViewed()))
-                    {
-                        validLocations.Add(new KeyValuePair<string, string>(location.NameOrUniqueName, location.DisplayName));
-                    }
+                    validLocations.Add(new KeyValuePair<string, string>(location.NameOrUniqueName, location.DisplayName));
                 }
-                if (validLocations.Count == 0)
-                {
-                    Farm farm = Game1.getFarm();
-
-                    validLocations.Add(new KeyValuePair<string, string>(farm.NameOrUniqueName, farm.DisplayName));
-                }
-                Game1.currentLocation.ShowPagedResponses(I18n.Message("ChooseAnimalLocation"), validLocations, (string value) =>
-                {
-                    GameLocation locationFromName = Game1.getLocationFromName(value);
-
-                    if (locationFromName != null)
-                    {
-                        TargetLocation = locationFromName;
-                        if (Game1.activeClickableMenu is DialogueBox dialogueBox && dialogueBox is not null)
-                        {
-                            dialogueBox.closeDialogue();
-                        }
-                        Game1.activeClickableMenu = __instance;
-                        Game1.globalFadeToBlack(__instance.prepareForAnimalPlacement);
-                    }
-                    else
-                    {
-                        ModEntry.ModMonitor.Log("Can't find location '" + value + "' for animal relocate menu.", LogLevel.Error);
-                    }
-                }, auto_select_single_choice: true);
-                return false;
             }
-            return true;
+            if (validLocations.Count == 0)
+            {
+                Farm farm = Game1.getFarm();
+
+                validLocations.Add(new KeyValuePair<string, string>(farm.NameOrUniqueName, farm.DisplayName));
+            }
+            Game1.currentLocation.ShowPagedResponses(I18n.Message("ChooseAnimalLocation"), validLocations, value =>
+            {
+                GameLocation locationFromName = Game1.getLocationFromName(value);
+
+                if (locationFromName != null)
+                {
+                    TargetLocation = locationFromName;
+                    if (Game1.activeClickableMenu is DialogueBox dialogueBox)
+                    {
+                        dialogueBox.closeDialogue();
+                    }
+                    Game1.activeClickableMenu = __instance;
+                    Game1.globalFadeToBlack(__instance.prepareForAnimalPlacement);
+                }
+                else
+                {
+                    ModEntry.ModMonitor.Log("Can't find location '" + value + "' for animal relocate menu.", LogLevel.Error);
+                }
+            }, auto_select_single_choice: true);
+            return false;
         }
 
         // Update current location to TargetLocation
         private static void PrepareForAnimalPlacementPostfix(AnimalQueryMenu __instance)
         {
-            if (!ModEntry.Config.EnableAnimalRelocate)
-                return;
-            typeof(AnimalQueryMenu).GetField("movingAnimal", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(__instance, true);
-            FarmAnimal animal = (FarmAnimal)typeof(AnimalQueryMenu).GetField("animal", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance);
+            if (!ModEntry.Config.EnableAnimalRelocate) return;
+            typeof(AnimalQueryMenu).GetField("movingAnimal", BindingFlags.NonPublic | BindingFlags.Instance)!.SetValue(__instance, true);
             Game1.currentLocation.cleanupBeforePlayerExit();
             Game1.currentLocation = TargetLocation;
             Game1.player.viewingLocation.Value = Game1.currentLocation.NameOrUniqueName;
@@ -154,28 +151,28 @@ namespace AnythingAnywhere.Framework.Patches.Menus
             if (!ModEntry.Config.EnableAnimalRelocate)
                 return;
 
-            bool movingAnimal = (bool)typeof(AnimalQueryMenu).GetField("movingAnimal", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance);
-            FarmAnimal animal = (FarmAnimal)typeof(AnimalQueryMenu).GetField("animal", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance);
+            bool movingAnimal = (bool)typeof(AnimalQueryMenu).GetField("movingAnimal", BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(__instance)!;
+            FarmAnimal animal = (FarmAnimal)typeof(AnimalQueryMenu).GetField("animal", BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(__instance);
 
-            if (movingAnimal && TargetLocation != Game1.getFarm())
+            if (!movingAnimal || TargetLocation.Equals(Game1.getFarm())) return;
+
+            Vector2 clickTile = new((float)(Game1.viewport.X + Game1.getOldMouseX(ui_scale: false)) / 64, (float)(Game1.viewport.Y + Game1.getOldMouseY(ui_scale: false)) / 64);
+            foreach (Building building in TargetLocation.buildings)
             {
-                Vector2 clickTile = new((Game1.viewport.X + Game1.getOldMouseX(ui_scale: false)) / 64, (Game1.viewport.Y + Game1.getOldMouseY(ui_scale: false)) / 64);
-                foreach (Building building in TargetLocation.buildings)
-                {
-                    building.color = Color.White;
-                }
-                Building selection = TargetLocation.getBuildingAt(clickTile);
-                if (selection != null)
-                {
-                    if (animal.CanLiveIn(selection) && !((AnimalHouse)selection.GetIndoors()).isFull() && !selection.Equals(animal.home))
-                    {
-                        selection.color = Color.LightGreen * 0.8f;
-                    }
-                    else
-                    {
-                        selection.color = Color.Red * 0.8f;
-                    }
-                }
+                building.color = Color.White;
+            }
+            Building selection = TargetLocation.getBuildingAt(clickTile);
+
+            if (selection == null) return;
+            if (animal is null) return;
+
+            if (animal.CanLiveIn(selection) && !((AnimalHouse)selection.GetIndoors()).isFull() && !selection.Equals(animal.home))
+            {
+                selection.color = Color.LightGreen * 0.8f;
+            }
+            else
+            {
+                selection.color = Color.Red * 0.8f;
             }
         }
 
@@ -185,7 +182,7 @@ namespace AnythingAnywhere.Framework.Patches.Menus
             if (!ModEntry.Config.EnableAnimalRelocate)
                 return;
 
-            bool movingAnimal = (bool)typeof(AnimalQueryMenu).GetField("movingAnimal", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance);
+            bool movingAnimal = (bool)typeof(AnimalQueryMenu).GetField("movingAnimal", BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(__instance)!;
 
             if (movingAnimal)
             {
