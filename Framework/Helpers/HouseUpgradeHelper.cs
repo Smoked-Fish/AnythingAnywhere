3cb1ae492304d416d323806df3fc96823bdbc737
Smoked Fish <espy.jacob@gmail.com>
2024-06-10 05:50:00 -0700

    Cabin upgrade helper

---
+++ Framework/Helpers/HouseUpgradeHelper.cs
@@ -0,0 +1,199 @@
+﻿using StardewModdingAPI;
+using StardewValley;
+using StardewValley.BellsAndWhistles;
+using StardewValley.Locations;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnythingAnywhere.Framework.Helpers
+{
+    internal static class HouseUpgradeHelper
+    {
+        public static void UpgradeCabinsResponses()
+        {
+            var cabinPageNames = GetCabinsToUpgrade();
+            Game1.currentLocation.ShowPagedResponses("Upgrade Cabin?", cabinPageNames, cabin =>
+            {
+                OfferCabinUpgrade(cabin);
+                ModEntry.ModMonitor.Log(cabin, LogLevel.Debug);
+            }, true);
+        }
+
+        private static void OfferCabinUpgrade(string cabin)
+        {
+            string msg;
+            switch (Game1.player.HouseUpgradeLevel)
+            {
+                case 0:
+                    msg = Game1.content.LoadString("Strings\\Locations:ScienceHouse_Carpenter_UpgradeHouse1");
+                    msg = ModEntry.Config.EnableFreeBuild ? msg.Replace("10,000", "0").Replace("10.000", "0").Replace("10 000", "0").Replace("450", "0") : msg;
+                    Game1.currentLocation.createQuestionDialogue(Game1.parseText(msg), Game1.currentLocation.createYesNoResponses(), (_, answer) =>
+                            {
+                                if (answer == "Yes")
+                                    AcceptCabinUpgrade(cabin);
+                            }
+                        );
+                    break;
+                case 1:
+                    msg = Game1.content.LoadString("Strings\\Locations:ScienceHouse_Carpenter_UpgradeHouse2", ModEntry.Config.EnableFreeBuild ? "0" : "65,000", ModEntry.Config.EnableFreeBuild ? "0" : "100");
+                    Game1.currentLocation.createQuestionDialogue(Game1.parseText(msg), Game1.currentLocation.createYesNoResponses(), (_, answer) =>
+                            {
+                                if (answer == "Yes")
+                                    AcceptCabinUpgrade(cabin);
+                            }
+                        );
+                    break;
+                case 2:
+                    msg = Game1.content.LoadString("Strings\\Locations:ScienceHouse_Carpenter_UpgradeHouse3");
+                    msg = ModEntry.Config.EnableFreeBuild ? msg.Replace("10,000", "0").Replace("10.000", "0").Replace("100,000", "0").Replace("100.000", "0").Replace("100 000", "0") : msg;
+                    Game1.currentLocation.createQuestionDialogue(Game1.parseText(msg), Game1.currentLocation.createYesNoResponses(), (_, answer) =>
+                            {
+                                if (answer == "Yes")
+                                    AcceptCabinUpgrade(cabin);
+                            }
+                            );
+                    break;
+            }
+        }
+
+        private static void AcceptCabinUpgrade(string cabin)
+        {
+            var cabinBuilding = Game1.getLocationFromName(cabin).GetContainingBuilding();
+            var cabinInstance = (Cabin)cabinBuilding.indoors.Value;
+
+            if (cabinInstance == null)
+            {
+                ModEntry.ModMonitor.Log("Could not find cabin location", LogLevel.Error);
+                return;
+            }
+
+            if (ModEntry.Config.EnableFreeBuild)
+            {
+                cabinBuilding.daysUntilUpgrade.Value = ModEntry.Config.EnableInstantBuild ? 0 : 3;
+                Game1.RequireCharacter("Robin").setNewDialogue("Data\\ExtraDialogue:Robin_HouseUpgrade_Accepted");
+                Game1.drawDialogue(Game1.getCharacterFromName("Robin"));
+                ModEntry.Multiplayer?.globalChatInfoMessage("HouseUpgrade", Game1.player.Name, Lexicon.getTokenizedPossessivePronoun(Game1.player.IsMale));
+                CompleteHouseUpgrade(cabinInstance.owner);
+                return;
+            }
+
+            switch (cabinInstance.upgradeLevel)
+            {
+                case 0:
+                    if (Game1.player.Money >= 10000 && Game1.player.Items.ContainsId("(O)388", 450))
+                    {
+                        cabinBuilding.daysUntilUpgrade.Value = 3;
+                        Game1.player.Money -= 10000;
+                        Game1.player.Items.ReduceId("(O)388", 450);
+                        Game1.RequireCharacter("Robin").setNewDialogue("Data\\ExtraDialogue:Robin_HouseUpgrade_Accepted");
+                        Game1.drawDialogue(Game1.getCharacterFromName("Robin"));
+                        ModEntry.Multiplayer?.globalChatInfoMessage("HouseUpgrade", Game1.player.Name, Lexicon.getTokenizedPossessivePronoun(Game1.player.IsMale));
+                        CompleteHouseUpgrade(cabinInstance.owner);
+                    }
+                    else if (Game1.player.Money < 10000)
+                    {
+                        Game1.drawObjectDialogue(Game1.content.LoadString("Strings\\UI:NotEnoughMoney3"));
+                    }
+                    else
+                    {
+                        Game1.drawObjectDialogue(Game1.content.LoadString("Strings\\Locations:ScienceHouse_Carpenter_NotEnoughWood1"));
+                    }
+
+                    break;
+                case 1:
+                    if (Game1.player.Money >= 65000 && Game1.player.Items.ContainsId("(O)709", 100))
+                    {
+                        cabinBuilding.daysUntilUpgrade.Value = 3;
+                        Game1.player.Money -= 65000;
+                        Game1.player.Items.ReduceId("(O)709", 100);
+                        Game1.RequireCharacter("Robin").setNewDialogue("Data\\ExtraDialogue:Robin_HouseUpgrade_Accepted");
+                        Game1.drawDialogue(Game1.getCharacterFromName("Robin"));
+                        ModEntry.Multiplayer?.globalChatInfoMessage("HouseUpgrade", Game1.player.Name, Lexicon.getTokenizedPossessivePronoun(Game1.player.IsMale));
+                        CompleteHouseUpgrade(cabinInstance.owner);
+                    }
+                    else if (Game1.player.Money < 65000)
+                    {
+                        Game1.drawObjectDialogue(Game1.content.LoadString("Strings\\UI:NotEnoughMoney3"));
+                    }
+                    else
+                    {
+                        Game1.drawObjectDialogue(Game1.content.LoadString("Strings\\Locations:ScienceHouse_Carpenter_NotEnoughWood2", "100"));
+                    }
+
+                    break;
+                case 2:
+                    if (Game1.player.Money >= 100000)
+                    {
+                        cabinBuilding.daysUntilUpgrade.Value = 3;
+                        Game1.player.Money -= 100000;
+                        Game1.RequireCharacter("Robin").setNewDialogue("Data\\ExtraDialogue:Robin_HouseUpgrade_Accepted");
+                        Game1.drawDialogue(Game1.getCharacterFromName("Robin"));
+                        ModEntry.Multiplayer?.globalChatInfoMessage("HouseUpgrade", Game1.player.Name, Lexicon.getTokenizedPossessivePronoun(Game1.player.IsMale));
+                        CompleteHouseUpgrade(cabinInstance.owner);
+                    }
+                    else if (Game1.player.Money < 100000)
+                    {
+                        Game1.drawObjectDialogue(Game1.content.LoadString("Strings\\UI:NotEnoughMoney3"));
+                    }
+
+                    break;
+            }
+        }
+
+        private static List<KeyValuePair<string, string>>? GetCabinsToUpgrade()
+        {
+            if (!Game1.IsMasterGame)
+                return null;
+
+            var cabinBuildings = GetCabins();
+
+            List<KeyValuePair<string, string>> cabinPageNames = [];
+            foreach (var cabin in cabinBuildings)
+            {
+                string msg = Game1.content.LoadString("Strings\\Buildings:Cabin_Name");
+                if (string.IsNullOrEmpty(cabin.owner.Name))
+                {
+                    msg = $"Empty {msg}";
+                    cabinPageNames.Add(new KeyValuePair<string, string>(cabin.uniqueName.Value, msg));
+                }
+                else
+                {
+                    msg = $"{cabin.owner.displayName}'s {msg}";
+                    cabinPageNames.Add(new KeyValuePair<string, string>(cabin.uniqueName.Value, msg));
+                }
+            }
+
+            return cabinPageNames;
+        }
+
+
+        private static List<Cabin> GetCabins()
+        {
+            List<Cabin> cabins = [];
+
+            foreach (var location in Game1.locations)
+            {
+                var locationCabins = location.buildings.Where(building => building.isCabin);
+                cabins.AddRange(locationCabins.Select(cabin => (Cabin)cabin.indoors.Value));
+            }
+
+            return cabins;
+        }
+
+
+
+        public static void CompleteHouseUpgrade(Farmer owner)
+        {
+            if (!ModEntry.Config.EnableInstantBuild)
+                return;
+
+            var homeOfFarmer = Utility.getHomeOfFarmer(owner);
+            homeOfFarmer.moveObjectsForHouseUpgrade(owner.HouseUpgradeLevel + 1);
+            owner.HouseUpgradeLevel++;
+            owner.daysUntilHouseUpgrade.Value = -1;
+            homeOfFarmer.setMapForUpgradeLevel(owner.HouseUpgradeLevel);
+            owner.stats.checkForBuildingUpgradeAchievements();
+            owner.autoGenerateActiveDialogueEvent("houseUpgrade_" + owner.HouseUpgradeLevel);
+        }
+    }
+}
